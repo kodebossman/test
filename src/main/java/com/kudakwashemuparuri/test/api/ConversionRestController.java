4790a5f85f5d852a72926ee4fe654a6edc4b4a4b
bossman001 <kuumuparuri@gmail.com>
2020-01-11 16:01:34 +0200

    km to miles conversion done

--- src/main/java/com/kudakwashemuparuri/test/api/ConversionRestController.java
+++ src/main/java/com/kudakwashemuparuri/test/api/ConversionRestController.java
@@ -5,6 +5,7 @@ import com.kudakwashemuparuri.test.service.ConversionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,15 +13,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConversionRestController {
 
 
-
+    // Service Injection
     private final ConversionsService conversionsServise;
 
     public ConversionRestController(ConversionsService conversionsServise) {
         this.conversionsServise = conversionsServise;
     }
 
+
+    // method convert to Celcius Controller Method
     @PostMapping("/ktoc")
-    public double convertKelvinToCelcious(Double kelvin) {
+    public double convertKelvinToCelcious(@RequestParam Double kelvin) {
         return conversionsServise.convertKelvinToCelcious(kelvin);
     }
+
+
+    //method convert to kelvins
+
+    @PostMapping("/ktoc")
+    public double convertCelciusToKelvin(@RequestParam Double celcius) {
+        return conversionsServise.convertCelciusToKelvin(celcius);
+    }
+
+    @PostMapping("/mtok")
+    public double convertMilesToKm(Double miles) {
+        return conversionsServise.milesTokm(miles);
+    }
+
+    @PostMapping("/kTom")
+    public double kmTomiles(Double km) {
+        return conversionsServise.kmTomiles(km);
+    }
+
+
 }
--- src/main/java/com/kudakwashemuparuri/test/service/ConversionsService.java
+++ src/main/java/com/kudakwashemuparuri/test/service/ConversionsService.java
@@ -7,6 +7,21 @@ import org.springframework.stereotype.Service;
 public class ConversionsService {
     public double convertKelvinToCelcious(Double kelvin) {
 
-        return kelvin- 273.15F;
+        return kelvin - 273.15F;
     }
+
+
+    public double convertCelciusToKelvin(Double celcious) {
+
+        return celcious + 273.15F;
+    }
+
+    public double milesTokm(double distanceInMiles) {
+        return distanceInMiles * 1.60934;
+    }
+
+    public double kmTomiles(double distanceInKm) {
+        return distanceInKm * 0.621371;
+    }
+
 }
